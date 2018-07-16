5f5058266ce6afc265a98b50c3e140ef751f0486
Amit Watve <amwat@google.com>
2018-07-16 16:38:40 -0700

    Add gubernator and bucket flags.
    Change-Id: Ibee789ce461bc8d84f04142e2b3663d777ca0701

--- prow/hack/verify-config.sh
+++ prow/hack/verify-config.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
 TESTINFRA_ROOT=$(git rev-parse --show-toplevel)
-PROW_GOB_CONFIG="${TESTINFRA_ROOT}/prow/gob/config.yaml"
-PROW_INTERNAL_CONFIG="${TESTINFRA_ROOT}/prow/internal/config.yaml"
+GUBERNATOR_URL="https://gubernator-internal.googleplex.com"
 
 cd /workspace/test-infra
 
-bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_GOB_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json="
-bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_INTERNAL_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json="
+# GoB Prow
+PROW_GOB_CONFIG="${TESTINFRA_ROOT}/prow/gob/config.yaml"
+PROW_GOB_BUCKET="gob-prow"
+
+bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_GOB_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json=" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_GOB_BUCKET}"
+
+# Internal prow
+PROW_INTERNAL_CONFIG="${TESTINFRA_ROOT}/prow/internal/config.yaml"
+PROW_INTERNAL_BUCKET="gke-prow"
+
+bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_INTERNAL_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json=" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_INTERNAL_BUCKET}"

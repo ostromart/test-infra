385dcfc1911d4af2d545443aeb32da02de58e3a3
Sen Lu <senlu@google.com>
2018-08-16 17:30:31 -0700

    skip certain job URL test
    Change-Id: I71cf413e818907754890c29df74c04041282a41b

--- prow/hack/verify-config.sh
+++ prow/hack/verify-config.sh
@@ -9,10 +9,10 @@ cd /workspace/test-infra
 PROW_GOB_CONFIG="${TESTINFRA_ROOT}/prow/gob/config.yaml"
 PROW_GOB_BUCKET="gob-prow"
 
-bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_GOB_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json=" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_GOB_BUCKET}"
+bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_GOB_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json=" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_GOB_BUCKET}" --test_arg="-k8s-prow=false"
 
 # Internal prow
 PROW_INTERNAL_CONFIG="${TESTINFRA_ROOT}/prow/internal/config.yaml"
 PROW_INTERNAL_BUCKET="gke-prow"
 
-bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_INTERNAL_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json=" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_INTERNAL_BUCKET}"
+bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_INTERNAL_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json=" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_INTERNAL_BUCKET}" --test_arg="-k8s-prow=false"

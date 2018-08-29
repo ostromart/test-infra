ae04cb2494d1c36da60050a420dc0037ae418cf0
Sen Lu <senlu@google.com>
2018-08-29 12:34:23 -0700

    gather status from both bazel command
    Change-Id: Ie3e4353947a095e75d0bfa5ae3054c36d15df5ed

--- prow/hack/verify-config.sh
+++ prow/hack/verify-config.sh
@@ -5,14 +5,20 @@ GUBERNATOR_URL="https://gubernator-internal.googleplex.com"
 
 cd /workspace/test-infra
 
+# exit code
+EXIT=0
+
 # GoB Prow
 PROW_GOB_CONFIG="${TESTINFRA_ROOT}/prow/gob/config.yaml"
 PROW_GOB_BUCKET="gob-prow"
 
-bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_GOB_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json=" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_GOB_BUCKET}" --test_arg="-k8s-prow=false"
+bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_GOB_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json=" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_GOB_BUCKET}" --test_arg="-k8s-prow=false" || EXIT=$?
 
 # Internal prow
 PROW_INTERNAL_CONFIG="${TESTINFRA_ROOT}/prow/internal/config.yaml"
 PROW_INTERNAL_BUCKET="gke-prow"
 
-bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_INTERNAL_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json=" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_INTERNAL_BUCKET}" --test_arg="-k8s-prow=false"
+bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_INTERNAL_CONFIG}" --test_arg="-job-config=" --test_arg="-config-json=" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_INTERNAL_BUCKET}" --test_arg="-k8s-prow=false" || EXIT=$?
+
+# When both bazel command fail, we are choosing to return the second one.
+exit "${EXIT}"

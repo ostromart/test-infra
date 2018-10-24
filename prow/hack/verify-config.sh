8dcc6ebf3b2b7c9912e96f67d8af7b5edcd2be6e
Sen Lu <senlu@google.com>
2018-10-23 17:39:35 -0700

    split up config.yaml for gob-prow
    Change-Id: I9d6ca8b29b9cc15815855c7dc8f63193673d0847

--- prow/hack/verify-config.sh
+++ prow/hack/verify-config.sh
@@ -9,10 +9,10 @@ cd $GOPATH/src/k8s.io/test-infra
 EXIT=0
 
 # GoB Prow
-PROW_GOB_CONFIG="${TESTINFRA_ROOT}/prow/gob/config.yaml"
+PROW_GOB_ROOT="${TESTINFRA_ROOT}/prow/gob"
 PROW_GOB_BUCKET="gob-prow"
 
-bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_GOB_CONFIG}" --test_arg="-job-config=" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_GOB_BUCKET}" --test_arg="-k8s-prow=false" || EXIT=$?
+bazel test config/tests/jobs:go_default_test --test_arg="-config=${PROW_GOB_ROOT}/config.yaml" --test_arg="-job-config=${PROW_GOB_ROOT}/config" --test_arg="-gubernator-path=${GUBERNATOR_URL}" --test_arg="-bucket=${PROW_GOB_BUCKET}" --test_arg="-k8s-prow=false" || EXIT=$?
 
 # Internal prow
 PROW_INTERNAL_CONFIG="${TESTINFRA_ROOT}/prow/internal/config.yaml"
